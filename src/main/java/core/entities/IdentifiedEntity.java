9a1188f58bf7213c85bc80bdcbc235ebcb658060
alsa0116 <aleksei.savin@NetCracker.com>
2018-08-29 23:48:25 +0300

    sorted out TODO: child classes moved to separate tables

--- src/main/java/core/entities/IdentifiedEntity.java
+++ src/main/java/core/entities/IdentifiedEntity.java
@@ -3,10 +3,13 @@ package core.entities;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
 import lombok.Data;
 
 @Data
 @Entity
+@Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
 public abstract class IdentifiedEntity {
 
   @Id
