9782973b03d78f091f77d5af6c5fa6ce2efc0bfe
alsa0116 <aleksei.savin@NetCracker.com>
2018-09-02 16:40:51 +0300

    added some logs

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,13 +2,19 @@ import core.configuration.DIContainer;
 import core.configuration.DatabaseLauncher;
 import io.vertx.core.Future;
 import io.vertx.core.Vertx;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import rest.ServerLauncher;
 
 //// TODO: 23.08.2018 todo multimodule, probably get rid of guice dependency
 //// TODO: 23.08.2018 todo logs
+//@Slf4j
 public class Main {
 
+  private static Logger log = LoggerFactory.getLogger(Main.class);
+
   public static void main(String[] args) {
+    log.info("Start mybank application");
     DatabaseLauncher.run(5432, "pg-config");
     // need to initialize it before deploying verticle
     DIContainer.getInjector();
