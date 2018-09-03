1c4d683957c374c7763f64e75ccc2a977d37bfb3
alsa0116 <aleksei.savin@NetCracker.com>
2018-09-03 03:08:27 +0300

    fixed todo

--- src/main/java/com/github/savinskiy/converters/AccountConverter.java
+++ src/main/java/com/github/savinskiy/converters/AccountConverter.java
@@ -10,7 +10,7 @@ import com.github.savinskiy.rest.to.AccountTo;
 @Mapper(componentModel = "jsr330", uses = BalanceConverter.class)
 public interface AccountConverter extends EntityToConverter<Account, AccountTo> {
 
-  public final static AccountConverter INSTANCE = Mappers.getMapper(AccountConverter.class);
+  AccountConverter INSTANCE = Mappers.getMapper(AccountConverter.class);
 
   // TODO: 29.08.2018 move id mappping to parent class (later)
   @Mappings({
